b2f85a113e162eded3c3ae8b1d9b1533edf71933
Govindu Thejana <149306686+Govindu-Thejana@users.noreply.github.com>
2025-01-18 15:48:19 +0530

    Update pipeline.groovy

--- pipeline.groovy
+++ pipeline.groovy
@@ -2,7 +2,7 @@ pipeline {
     agent any
         stage('Run Python Script') {
             steps {
-                    bat 'python3 hello.py'
+                    bat 'python hello.py'
             }
         }
     }
